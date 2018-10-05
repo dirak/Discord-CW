b36a69146b78d88cfe586afc6c433b2b32e8a316
dirak <dirak.social@gmail.com>
2018-10-04 20:57:35 -0400

    works

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const Discord = require('discord.js')
 const fs = require('fs')
 const request = require('request')
 
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){    
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
+var download = function(uri, filename){
+	return new Promise((resolve, reject) => {
+		request.head(uri, function(err, res, body){    
+			request(uri).pipe(fs.createWriteStream(filename)).on('close', (err, response, body) => {
+				if(err) return reject(err)
+				resolve(filename)
+			})
+		})
+	})
+}
 
 
 const client = new Discord.Client()
 
 client.on('ready', () => {
 	console.log('Client is ready.')
+	/*client.guilds.forEach((guild) => {
+		if(guild.id == '497427504232005634')console.log(guild.channels.values())
+	})*/
 })
 
 client.login('MzUwMDY0NzU3MTg2NzU2NjE4.DpexnA.P17eWG6TIHmiuIXzNgXSHknLHeI')
@@ -21,29 +29,32 @@ client.on('message', message => {
 	if(message.content.startsWith('!cw')) {
 		let cw = message.content.split('!cw')[1]
 		let user = message.author
-		let fields = message.attachments.map((a) => {
-			download(a.url, `./files/${a.filename}`, () => {
-
-			});
-			return {
-				"name": `${a.filename}`,
-				"value": `<${a.url}>`
-			}
-		})
-
-		message.delete(500)
-		message.channel.send({
-			embed: {
-				"title": "This is an auto-generated attachment for content warnings",
-				"description": cw,
-				"color": 13148428,
-				"timestamp": Date.now(),
-				"author": {
-					"name": user.username,
-					"icon_url": user.avatarURL
-				},
-				"fields": fields
-			}
+		let attachment = message.attachments.first()
+		let original_message = message
+		download(attachment.url, `./files/${attachment.filename}`)
+		.then((filename) => {
+			message.delete(500)
+			return client.channels.get('497559630407532544').send({file: filename})
+		}).then((message) => {
+			let attachment = message.attachments.first()
+			original_message.channel.send({
+				embed: {
+					"title": "This is an auto-generated attachment for content warnings",
+					"description": cw,
+					"color": 13148428,
+					"timestamp": Date.now(),
+					"author": {
+						"name": user.username,
+						"icon_url": user.avatarURL
+					},
+					"fields": [{
+						"name": "image",
+						"value": `${attachment.url}`
+					}]
+				}
+			})
+		}).catch((err) => {
+			console.log(err)
 		})
 	}
 	else if(message.content === '!dab') {
